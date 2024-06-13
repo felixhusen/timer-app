645cd641132c8214dc4a0e5613869ab97bb96d4e
Felix Husen <felix.husen@auxilis.com.au>
2024-06-14 08:46:16 +1000

    changed apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   stage: 'dev',
   production: false,
-  apiUrl: 'http://localhost:3000',
-  wsUrl: 'ws://localhost:3000',
+  apiUrl: 'http://ec2-52-62-92-200.ap-southeast-2.compute.amazonaws.com',
+  wsUrl: 'ws://ec2-52-62-92-200.ap-southeast-2.compute.amazonaws.com',
   minTimerDuration: 1,
   maxTimerDuration: 100,
 };
