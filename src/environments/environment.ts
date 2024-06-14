bad44732031b39513277eda2d88bad43d4243289
Felix Husen <felix.husen@auxilis.com.au>
2024-06-14 16:04:55 +1000

    fix: changed min and max timer duration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: false,
   apiUrl: 'http://ec2-52-62-92-200.ap-southeast-2.compute.amazonaws.com',
   wsUrl: 'ws://ec2-52-62-92-200.ap-southeast-2.compute.amazonaws.com',
-  minTimerDuration: 1,
-  maxTimerDuration: 100,
+  minTimerDuration: 600,
+  maxTimerDuration: 1200,
   snackbarDuration: 2000,
 };
